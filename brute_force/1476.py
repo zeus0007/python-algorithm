c089f09e30e42d22db9cd8e776cc271743f64712
zeus0007 <wpdn79@likelion.org>
2020-09-29 16:09:50 +0900

    백준 1476 날짜계산

---
+++ brute_force/1476.py
@@ -0,0 +1,97 @@
+# 문제
+# 준규가 사는 나라는 우리가 사용하는 연도와 다른 방식을 이용한다. 준규가 사는 나라에서는 수 3개를 이용해서 연도를 나타낸다. 각각의 수는 지구, 태양, 그리고 달을 나타낸다.
+
+# 지구를 나타내는 수를 E, 태양을 나타내는 수를 S, 달을 나타내는 수를 M이라고 했을 때, 이 세 수는 서로 다른 범위를 가진다. (1 ≤ E ≤ 15, 1 ≤ S ≤ 28, 1 ≤ M ≤ 19)
+
+# 우리가 알고있는 1년은 준규가 살고있는 나라에서는 1 1 1로 나타낼 수 있다. 1년이 지날 때마다, 세 수는 모두 1씩 증가한다. 만약, 어떤 수가 범위를 넘어가는 경우에는 1이 된다.
+
+# 예를 들어, 15년은 15 15 15로 나타낼 수 있다. 하지만, 1년이 지나서 16년이 되면 16 16 16이 아니라 1 16 16이 된다. 이유는 1 ≤ E ≤ 15 라서 범위를 넘어가기 때문이다.
+
+# E, S, M이 주어졌고, 1년이 준규가 사는 나라에서 1 1 1일때, 준규가 사는 나라에서 E S M이 우리가 알고 있는 연도로 몇 년인지 구하는 프로그램을 작성하시오.
+
+# 입력
+# 첫째 줄에 세 수 E, S, M이 주어진다. 문제에 나와있는 범위를 지키는 입력만 주어진다.
+
+# 출력
+# 첫째 줄에 E S M으로 표시되는 가장 빠른 연도를 출력한다. 1 1 1은 항상 1이기 때문에, 정답이 음수가 나오는 경우는 없다.
+
+# 예제 입력 & 출력
+# 1 16 16 -> 16
+# 1 1 1 -> 1
+# 1 2 3 -> 5266
+# 15 28 19 -> 7980
+E, S, M = input().split()
+E = int(E)
+S = int(S)
+M = int(M)
+
+def calc(e,s,m):
+    i = 1
+    if e == 15:
+        e = 0
+    if s == 28:
+        s = 0
+    if m == 19:
+        m = 0
+    while True :
+        if (i % 15 == e) and (i % 28 == s) and (i % 19 == m) :
+            print(i)
+            break
+        else :
+            i =  i + 1
+
+calc(E,S,M)
+
+# gcd lcm을 활용해서 풀기위해 시도 했었음.
+
+# def lcm(a, b, gcd_result):
+#     result = 1 
+#     result = a * b / gcd_result
+#     return result
+
+# def gcd (a,b):
+#     a_cd = []
+#     b_cd = []
+#     result = 1
+#     if a > b:
+#         for i in range(1, b + 1):
+#             if a % i == 0 :
+#                 a_cd.append(i)
+#             if b % i == 0 :
+#                 b_cd.append(i)
+#         a_cd.append(a)
+        
+#     else :
+#         for i in range(1, a + 1):
+#             if a % i == 0 :
+#                 a_cd.append(i)
+#             if b % i == 0 :
+#                 b_cd.append(i)
+#         b_cd.append(b)
+#     print(a_cd)
+#     print(b_cd)
+
+#     for b_item in b_cd:
+#         if b_item in a_cd:
+#             result = result * b_item
+#     return result
+
+# # gcd_result = gcd(aa,bb)
+# # print(a_cd)
+# # print(b_cd)
+# # lcm(aa, bb, gcd_result)
+
+
+# # 16 29
+
+# earth_year = E + 15
+# sun_year = S + 28
+# moon_year = M + 19
+# e_s_gcd = gcd(earth_year, sun_year)
+# print(e_s_gcd)
+# e_s_lcm = lcm(earth_year, sun_year, e_s_gcd)
+# print(e_s_lcm)
+# e_s_m_gcd = gcd(e_s_lcm, moon_year)
+# print(e_s_m_gcd)
+# e_s_m_lcm = lcm(e_s_lcm, moon_year, e_s_m_gcd)
+# print(e_s_m_lcm)
